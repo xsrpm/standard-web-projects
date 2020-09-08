3be79e59999549eb8a771e9d71509982d0858767
César Palma Morante <cemp2703@gmail.com>
2020-09-08 16:25:35 -0500

    todo-list html template
    Version mejorada a la de app.js, usando html templates

--- js/todo-list/js/app-template.js
+++ js/todo-list/js/app-template.js
@@ -1,15 +1,14 @@
-/*Metodo: Insertando html como String dentro del JS
-- localStorage (salvando y recuperando un array de objetos)
-- change css classes
-- add events to dinamicaly created elements
-- polifill to ChildNode.remove() (para soportar ie explorer )
-*/
+/**
+ * Version mejorada a la de app.js, usando html templates
+ * 
+ */
 
 (function(){
     const itemForm = document.getElementById("itemForm");
     const itemInput = document.getElementById("itemInput");
     const clearList = document.getElementById("clear-list");
     const itemList = document.getElementsByClassName("item-list my-5")[0];
+    const itemTemplate = document.getElementById("itemTemplate");
     let arrData;
     function listStorage(){
         let json = JSON.parse(localStorage.getItem('data'));
@@ -19,20 +18,6 @@
         });
     }
     listStorage();
-    function ItemString(item){
-        const cad =`
-        <!-- single item -->
-        <div class="item my-3">
-      <h5 class="item-name text-capitalize ${item.completed?"completed":""}">${item.value}</h5>
-      <div class="item-icons">
-       <a href="#" class="complete-item mx-2 item-icon ${item.completed?"visibility":""}"><i class="far fa-check-circle"></i></a>
-       <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
-       <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
-      </div>
-     </div>
-     <!-- end of single item -->`;
-     return cad;
-    }
     function saveItemstoLocalStorage(){
         arrData = [];
         const itemName = document.getElementsByClassName("item-name");
@@ -42,9 +27,13 @@
         localStorage.setItem('data', JSON.stringify(arrData));
     }
     function addItem(item){
-        itemList.insertAdjacentHTML("beforeend",ItemString(item));
-        const itemIcons = itemList.lastElementChild.lastElementChild;
-        itemIcons.children[0].addEventListener("click",(e)=>{
+        const itemmy3 = itemTemplate.content.cloneNode(true).firstElementChild;
+        itemmy3.children[0].innerText=item.value;
+        if(item.completed){
+            itemmy3.children[0].classList.add("completed");
+            itemmy3.children[1].children[0].classList.add("visibility");
+        }
+        itemmy3.children[1].children[0].addEventListener("click",(e)=>{
             let anchor=e.target;
             while(anchor.tagName!=="A"){
                 anchor=anchor.parentElement;
@@ -53,8 +42,8 @@
             anchor.classList.toggle("visibility");
             anchor.parentElement.previousElementSibling.classList.toggle("completed");
             saveItemstoLocalStorage();
-        })
-        itemIcons.children[1].addEventListener("click",(e)=>{
+        });      
+        itemmy3.children[1].children[1].addEventListener("click",(e)=>{
             let anchor=e.target;
             while(anchor.tagName!=="A"){
                 anchor=anchor.parentElement;
@@ -62,16 +51,18 @@
             itemInput.value= anchor.parentElement.previousElementSibling.innerText;
             anchor.parentElement.parentElement.remove();
             saveItemstoLocalStorage();
-        })
-        itemIcons.children[2].addEventListener("click",(e)=>{
+        });   
+        itemmy3.children[1].children[2].addEventListener("click",(e)=>{
             let anchor=e.target;
             while(anchor.tagName!=="A"){
                 anchor=anchor.parentElement;
             }
             anchor.parentElement.parentElement.remove();
             saveItemstoLocalStorage();
-        })
+        });
+        itemList.appendChild(itemmy3);
     }
+
     itemForm.addEventListener("submit",(e)=>{
         e.preventDefault();
         arrData.push({value:itemInput.value,completed:false});
@@ -83,7 +74,7 @@
         itemList.innerHTML="";
         localStorage.setItem('data', JSON.stringify([]));
     });
-})();
+})()
 
 
 /*
