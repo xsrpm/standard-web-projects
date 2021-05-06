34876454ba64a17b3987f9df93e57e3ee8b74eb3
César Palma Morante <cemp2703@gmail.com>
2021-05-06 00:35:29 -0500

    fix # para home

--- js/cientifico/src/templates/header.js
+++ js/cientifico/src/templates/header.js
@@ -3,7 +3,7 @@ const Header = () =>{
         <div class="Header-main">
             <div class="Header-Logo">
                 <h1>
-                    <a href="/">
+                    <a href="#">
                         100tifi.co
                     </a>
                 </h1>
