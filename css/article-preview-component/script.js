edcac10acb16507260cc9776f54f4ac75a6e9502
César Palma Morante <cemp2703@gmail.com>
2021-09-21 09:34:43 -0500

    article-preview-component - active code js

--- css/article-preview-component/script.js
+++ css/article-preview-component/script.js
@@ -1,1 +1,9 @@
+console.log('script')
+
 const shareButton = document.querySelector('.card .share-button')
+const shareSection = document.querySelector('.card .share-section')
+shareButton.addEventListener('click', () => {
+  console.log('holaaaa')
+  shareSection.classList.toggle('hide')
+  shareButton.classList.toggle('share-button-active')
+})
